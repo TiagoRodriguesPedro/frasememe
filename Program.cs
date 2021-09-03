bd0bd28aaf976531d458281fbdc0b684830cf7a8
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-03 10:34:20 -0300

    resoluçao exerciçio frasememe

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace frasememe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada, frasememetonervoso;
+            Console.WriteLine("O que voce acha de mim ?");
+             frasedigitada = Console.ReadLine();
+
+          frasememetonervoso = $"{frasedigitada.Remove(10)}...Náo Náo pera, To Nervoso";
+
+            Console.WriteLine(frasememetonervoso);
+
+        }
+    }
+}
